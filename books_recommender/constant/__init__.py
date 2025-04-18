feaee6b539436ed62cbb03da78fdd9fa860773bb
Robin Raj <robinrajinfo@gmail.com>
2025-04-18 17:56:51 +0530

    data ingestion updated

---
+++ books_recommender/constant/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+ROOT_DIR = os.getcwd()
+# Main config file path
+CONFIG_FOLDER_NAME = "config"
+CONFIG_FILE_NAME = "config.yaml"
+CONFIG_FILE_PATH = os.path.join(ROOT_DIR,CONFIG_FILE_PATH,CONFIG_FILE_NAME)
